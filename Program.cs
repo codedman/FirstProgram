f4ba4ba2f54a29a989f28caf7da201b23c9c83c6
codedman <obasiuche62@gmail.com>
2018-07-12 23:25:58 +0100

    added my c# code

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A;
+            int B;
+            float Result;
+            Console.Write("\n Enter the value A: ");
+            A = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n Enter the value B: ");
+            B = Convert.ToInt32(Console.ReadLine());
+            Result = A + B;
+            Console.Write("\n The Answer is: " + Result);
+            Console.ReadLine();
+        }
+    }
+}
